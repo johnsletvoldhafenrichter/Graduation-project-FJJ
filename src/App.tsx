dc814d19a188e89967dbcca8ccdb37198347739c
GitJarand <79191936+GitJarand@users.noreply.github.com>
2021-04-27 14:29:52 +0200

    Fjernet .idea
    Fjernet .idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,59 @@
 import React from 'react';
 import './css/App.css';
 import "@dossier/mithra-ui/dist/ds.css"
-import {Button} from "@dossier/mithra-ui";
+import {
+    ApplicationLayout,
+    PageHeaderButton,
+    NavbarSection,
+    NavbarButton,
+} from "@dossier/mithra-ui";
 
 function App() {
     return (
         <div className="App ds-typography-body">
-            <Button variant="primary">
-                test
-            </Button>
-            <div>Mithra is imported !!!!</div>
+            <ApplicationLayout
+                backButtonTitle="Back to plans"
+                headerButtons={
+                    <>
+                        <PageHeaderButton
+                            icon="Search"
+                            text="Søk"/>
+                        <PageHeaderButton
+                            icon="Filter"
+                            text="Filter"/>
+                    </>
+                }
+                headerText="Min side"
+                navbarSections={
+                    <>
+                        <NavbarSection>
+                            {/*<NavbarButton*/}
+                            {/*    as={NavLink}*/}
+                            {/*    exact icon="UserHome"*/}
+                            {/*    text="Home"*/}
+                            {/*    to="/Navbar"/>*/}
+                            <NavbarButton
+                                icon="AdminHome"
+                                text="Min side"/>
+                            <NavbarButton
+                                icon="UserPlans"
+                                text="Mine kurs"/>
+                            <NavbarButton
+                                icon="Learn"
+                                text="Kursoversikt"/>
+                            <NavbarButton
+                                icon="User"
+                                text="Profil"/>
+                            <NavbarButton
+                                icon="Logout"
+                                text="Logg ut"/>
+                        </NavbarSection>
+                    </>
+                }
+                pageType="user"
+                tray={null}
+             isMainPage>
+            </ApplicationLayout>
         </div>
     );
 }
