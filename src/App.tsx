94e1a375dc6719cb6f995ab63c98ddb43440d489
tunestam <80259169+tunestam@users.noreply.github.com>
2021-04-27 16:57:55 +0200

    seperte filer til components, la til autentifikasjon, og la til lenk til 'min side'

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,27 @@
 import React from 'react';
 import './css/App.css';
 import "@dossier/mithra-ui/dist/ds.css"
-import {
-    ApplicationLayout,
-    PageHeaderButton,
-    NavbarSection,
-    NavbarButton,
-} from "@dossier/mithra-ui";
+import Main from './components/main';
 
-function App() {
-    return (
-        <div className="App ds-typography-body">
-            <ApplicationLayout
-                backButtonTitle="Back to plans"
-                headerButtons={
-                    <>
-                        <PageHeaderButton
-                            icon="Search"
-                            text="Søk"/>
-                        <PageHeaderButton
-                            icon="Filter"
-                            text="Filter"/>
-                    </>
-                }
-                headerText="Min side"
-                navbarSections={
-                    <>
-                        <NavbarSection>
-                            {/*<NavbarButton*/}
-                            {/*    as={NavLink}*/}
-                            {/*    exact icon="UserHome"*/}
-                            {/*    text="Home"*/}
-                            {/*    to="/Navbar"/>*/}
-                            <NavbarButton
-                                icon="AdminHome"
-                                text="Min side"/>
-                            <NavbarButton
-                                icon="UserPlans"
-                                text="Mine kurs"/>
-                            <NavbarButton
-                                icon="Learn"
-                                text="Kursoversikt"/>
-                            <NavbarButton
-                                icon="User"
-                                text="Profil"/>
-                            <NavbarButton
-                                icon="Logout"
-                                text="Logg ut"/>
-                        </NavbarSection>
-                    </>
-                }
-                pageType="user"
-                tray={null}
-             isMainPage>
-            </ApplicationLayout>
-        </div>
-    );
+class App extends React.Component {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            isAuthenticated: true,
+            isLoading: false,
+        };
+    }
+
+    render() {      
+        // @ts-ignore
+        const {isAuthenticated} = this.state;
+        return (
+            isAuthenticated ?
+            <Main/> :
+            <div>Ikke Logget Inn!!!</div>
+        )
+    }
 }
 
 export default App;
