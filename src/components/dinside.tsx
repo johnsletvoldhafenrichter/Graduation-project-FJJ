96b07d0a28292680dded6b895fc22b51d02a2d6e
GitJarand <79191936+GitJarand@users.noreply.github.com>
2021-04-30 18:29:39 +0200

    dinside cards

--- src/components/dinside.tsx
+++ src/components/dinside.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import {Card, H3, Text} from "@dossier/mithra-ui";
+import {Card, H3, H5, H6, SubTitle2, Text} from "@dossier/mithra-ui";
 const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 export class DinSide extends React.Component {
@@ -31,7 +31,7 @@ export class DinSide extends React.Component {
     //
     // }
 
-    handleClick(courseId: any, course_name: string) {
+    handleClick(courseId: any) {
         // @ts-ignore
         const {history} = this.props;
         history.push("/coursedetails/" + courseId);
@@ -51,28 +51,84 @@ export class DinSide extends React.Component {
 
         const courseCard = obligCourses
           // @ts-ignore
-          .map(({course_id, course_name, course_description, image_url}) => {
-              const imageStyles = {
-                  maxWidth: '200px',
-                  maxHeight: '133px'
-              }
+          .map(({course_id, course_name, image_url, start_date, end_date, org, enrollment_end}) => {
+            const cardStyle = {
+              maxWidth: '15em',
+              height: '20em',
+              margin: 10,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              padding: '5px'
+            };
+            const imageStyles = {
+              flex: 1,
+              maxWidth: '10em',
+              maxHeight: '7em',
+              margin: '10px 0px 10px 0px'
+            }
+            const H5Style = {
+              flex: 1,
+              minHeight: '2.5em',
+              margin: '0px',
+              fontSize: '14px',
+              fontWeight: 'bold'
+            }
+
+            const divDateStyles = {
+              flex: 1,
+              width: '100%',
+              display: 'flex',
+            };
+            const dateStyles = {
+              width: '50%',
+              margin: '0px 5px 0px 5px',
+              fontSize: '11px'
+            };
+            const dateTitleStyle = {
+              margin: '0px',
+              fontSize: '12px'
+            };
+            const H6Styles = {
+              margin: '0px',
+              fontSize: '12px'
+            };
+
               return (
                 <Card
-                  style={{
-                      maxWidth: '25em',
-                      margin: 10,
-                      flexDirection: 'row'
-                  }}
-                  onClick={(event) => this.handleClick(course_id, course_name)}
+                  // @ts-ignore
+                  style={cardStyle}
+                  onClick={(event) => this.handleClick(course_id)}
                 >
-                    <H3>OBLIG!</H3>
-                    <img alt="image" style={imageStyles} src={image_url}/>
-                    <H3>
-                        {course_name}
-                    </H3>
-                    <Text>
-                        {course_description}
+                  <img alt="image" style={imageStyles} src={image_url}/>
+                  <H5 style={H5Style}>
+                    {course_name}
+                  </H5>
+                  <div style={divDateStyles}>
+                    <Text style={dateStyles}>
+                      <SubTitle2 style={dateTitleStyle}>
+                        Start dato
+                      </SubTitle2>
+                      {start_date.slice(0, 10)}
                     </Text>
+                    <Text style={dateStyles}>
+                      <SubTitle2 style={dateTitleStyle}>
+                        Slutt dato
+                      </SubTitle2>
+                      {end_date.slice(0, 10)}
+                    </Text>
+                  </div>
+                  <div style={{margin:'5px 0px'}}>
+                    <H6 style={H6Styles}>
+                      Oppmeldingsfrist
+                    </H6>
+                    <H6 style={dateTitleStyle}>
+                      {enrollment_end.slice(0, 10)}
+                    </H6>
+                  </div>
+                  <Text style={H6Styles}>
+                    Tilbyder: {org}
+                  </Text>
                 </Card>
               );
           });
