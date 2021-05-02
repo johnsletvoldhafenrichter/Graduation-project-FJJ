ca36d950834ee73f275e5843f1ded0875acce593
tunestam <80259169+tunestam@users.noreply.github.com>
2021-05-02 16:17:55 +0200

    mineKurs now displays couseCards + prettification
    Prettification av kursoversikt.tsx
    Removed all styles in kursoversikt.tsx and moved them to courseCards.css
    mineKurs.tsx now displays started courses through a course card

--- src/components/kursoversikt.tsx
+++ src/components/kursoversikt.tsx
@@ -7,7 +7,6 @@ import '../css/courseCards.css';
 export class Kursoversikt extends React.Component {
   constructor(props: any) {
     super(props);
-
     this.state = {
       courses: [],
       error: null,
@@ -15,120 +14,82 @@ export class Kursoversikt extends React.Component {
   };
 
   async componentDidMount() {
-      try {
-          const courses = await getAllCourses();
-          if (!courses) {
-              this.setState({
-                  error: 'Could not find courses!'
-              })
-              return;
-          }
-          // @ts-ignore
-          this.setState({courses});
-      } catch (error) {
-          this.setState({
-              error: 'Something went wrong!'
-          })
-          return;
+    try {
+      const courses = await getAllCourses();
+      if (!courses) {
+        this.setState({
+          error: 'Could not find courses!'
+        })
+        return;
       }
+      // @ts-ignore
+      this.setState({courses});
+    } catch (error) {
+      this.setState({
+        error: 'Something went wrong!'
+      })
+      return;
+    }
   }
 
-    handleClick(courseId: number) {
-        // @ts-ignore
-        const {history} = this.props;
-        history.push("/coursedetails/" + courseId);
+  handleClick(courseId: number) {
+    // @ts-ignore
+    const {history} = this.props;
+    history.push("/coursedetails/" + courseId);
   }
 
   render() {
-    const {
-      // @ts-ignore
-      courses,
-      // @ts-ignore
-      courseDetails
-    } = this.state;
-    const courseStyles = {
-      display: 'flex',
-      flexWrap: 'wrap',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }
+    // @ts-ignore
+    const {courses, courseDetails} = this.state;
     // @ts-ignore
     const {error} = this.state
     if (error) {
-        return <div>{error}!</div>
+      return <div>{error}!</div>
     }
-
     const courseCard = courses
       // @ts-ignore
-      .map(({ course_id, course_name, image_url, start_date, end_date, org, enrollment_end}) => {
-
-        const divDateStyles = {
-            flex: 1,
-            width: '100%',
-            display: 'flex',
-        };
-        const dateStyles = {
-          width: '50%',
-          margin: '0px 5px 0px 5px',
-          fontSize: '11px'
-        };
-        const dateTitleStyle = {
-          margin: '0px',
-          fontSize: '12px'
-        };
-        const H6Styles = {
-          margin: '0px',
-          fontSize: '12px'
-        };
-
-
+      .map(({course_id, course_name, image_url, start_date, end_date, org, enrollment_end}) => {
         return (
-
-        <Card
-            className={'card'}
-            // @ts-ignore
-            onClick={(event) => this.handleClick(course_id)}
-        >
-          <img className={'imageStyles'} alt="image" src={image_url}/>
-          <H5 className={'H5Style'}>
-            {course_name}
-          </H5>
-          <div className={'dateContainer'}>
-            <Text className={'dateStyles'}>
-              <SubTitle2 className={'dateTitleStyle'}>
-                  Start dato
-              </SubTitle2>
-                {start_date.slice(0, 10)}
-            </Text>
-            <Text className={'dateStyles'}>
+          // @ts-ignore
+          <Card className={'card'} onClick={(event) => this.handleClick(course_id)}>
+            <img className={'imageStyles'} alt="image" src={image_url}/>
+            <H5 className={'H5Style'}>
+              {course_name}
+            </H5>
+            <div className={'dateContainer'}>
+              <Text className={'dateStyles'}>
                 <SubTitle2 className={'dateTitleStyle'}>
-                    Slutt dato
+                  Start dato
+                </SubTitle2>
+                {start_date.slice(0, 10)}
+              </Text>
+              <Text className={'dateStyles'}>
+                <SubTitle2 className={'dateTitleStyle'}>
+                  Slutt dato
                 </SubTitle2>
                 {end_date.slice(0, 10)}
-            </Text>
-          </div>
-          <div className={'enrollmentContainer'}>
-            <H6 className={'H6Styles'}>
-              Oppmeldingsfrist
-            </H6>
-            <H6 className={'dateTitleStyle'}>
-              {enrollment_end.slice(0, 10)}
-            </H6>
-          </div>
+              </Text>
+            </div>
+            <div className={'enrollmentContainer'}>
+              <H6 className={'H6Styles'}>
+                Oppmeldingsfrist
+              </H6>
+              <H6 className={'dateTitleStyle'}>
+                {enrollment_end.slice(0, 10)}
+              </H6>
+            </div>
             <Text className={'H6Styles'}>
               Tilbyder: {org}
             </Text>
-        </Card>
+          </Card>
         );
       });
-
-    return(
+    return (
       // @ts-ignore
       courseDetails ? <Kurs course={courseDetails}/> :
-        // @ts-ignore
-  <div style={courseStyles}>
-    {courseCard}
-  </div>
-  );
-}
+        <div className={'coursesContainer'}>
+          {courseCard}
+        </div>
+    );
+  }
 }
--- src/components/mineKurs.tsx
+++ src/components/mineKurs.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {getCourseById} from "../functions/kursFunctions";
+import {Card, H5, H6, SubTitle2, Text} from "@dossier/mithra-ui";
 
 
 const axios = require('axios');
@@ -12,9 +14,6 @@ export class MineKurs extends React.Component{
       error: null
     }
   };
-
-  //mycourses-endepunktet gir en 404, men funksjonen går gjennom med /courses som endepunkt.
-  // /mycourses skal console.log som første operasjon, men nei... wtf??
 
   async getStartedCourses(userId: Number) {
     const result: any = await axios(serverUrl + '/mycourses', {
@@ -40,7 +39,6 @@ export class MineKurs extends React.Component{
       }
       this.setState({myStartedCourses});
 
-      console.log(myStartedCourses);
     } catch (error) {
       this.setState({
         error: 'Component did not mount!'
@@ -50,15 +48,52 @@ export class MineKurs extends React.Component{
   }
 
   render() {
-
     // @ts-ignore
     const {myStartedCourses} = this.state;
+    const courseCard = myStartedCourses
+      // @ts-ignore
+      .map(({course_id, course_name, image_url, start_date, end_date, org, enrollment_end}) => {
+        return (
+          // @ts-ignore
+          <Card className={'card'} onClick={(event) => this.handleClick(course_id)}>
+            <img className={'imageStyles'} alt="image" src={image_url}/>
+            <H5 className={'H5Style'}>
+              {course_name}
+            </H5>
+            <div className={'dateContainer'}>
+              <Text className={'dateStyles'}>
+                <SubTitle2 className={'dateTitleStyle'}>
+                  Start dato
+                </SubTitle2>
+                {start_date.slice(0, 10)}
+              </Text>
+              <Text className={'dateStyles'}>
+                <SubTitle2 className={'dateTitleStyle'}>
+                  Slutt dato
+                </SubTitle2>
+                {end_date.slice(0, 10)}
+              </Text>
+            </div>
+            <div className={'enrollmentContainer'}>
+              <H6 className={'H6Styles'}>
+                Oppmeldingsfrist
+              </H6>
+              <H6 className={'dateTitleStyle'}>
+                {enrollment_end.slice(0, 10)}
+              </H6>
+            </div>
+            <Text className={'H6Styles'}>
+              Tilbyder: {org}
+            </Text>
+          </Card>
+        );
+      });
+
 
 
     return(
-
-      <div>
-        Her kommer alle de kule kursene dine!
+      <div className={'coursesContainer'}>
+        {courseCard}
       </div>
     );
   }
