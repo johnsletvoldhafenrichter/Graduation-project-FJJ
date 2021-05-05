58109d27cfdd4e249478648701c624023049bd95
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2021-05-05 16:50:31 +0200

    new f branch, almost done with filter

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,6 +1,6 @@
-import {Text, Tray, TrayHeader, Stack, Tab, Input, FormLabel} from "@dossier/mithra-ui";
+import {Text, Tray, TrayHeader, Stack, Tab, Input, FormLabel, Button} from "@dossier/mithra-ui";
 import React from "react";
-import {getFilteredCourses} from "../functions/filterFunctions";
+import {getFilteredCourses, getQueryResult} from "../functions/filterFunctions";
 
 export class Filter extends React.Component<any, any> {
     constructor(props: any) {
@@ -9,30 +9,86 @@ export class Filter extends React.Component<any, any> {
         this.state = {
             activeTab: null,
             error: '',
+            filtrationTags: [],
+            selectedTags: [],
+            inputField: '',
         }
     }
 
     async handleClick(fromWhere: string) {
         const path = window.location.pathname;
-        if (fromWhere !== this.state.activeTab) {
-            try {
-                let filteredCourses = await getFilteredCourses(fromWhere, path)
-                this.props.setFilteredCourses(filteredCourses)
-            } catch (err) {
-                this.setState({error: 'Couldnt get courses'})
-            }
+        // if (fromWhere !== this.state.activeTab) {
+        try {
+            let filtrationTags = await getFilteredCourses(fromWhere, path)
+            this.setState({filtrationTags})
+            // this.props.setFilteredCourses(filteredCourses)
+        } catch (err) {
+            this.setState({error: 'Couldnt get courses'})
         }
+        // }
         this.setState({
             activeTab: fromWhere,
         })
     }
 
-    componentDidMount() {
+    handleAddTag(event: any) {
+        const id = parseInt(event.target.id)
+        const selectedTags = this.state.selectedTags
+        if (!selectedTags.includes(id)) {
+            selectedTags.push(id)
+        }
+        this.setState({selectedTags})
+    }
+
+    handleRemoveTag(event: any) {
+        const selectedTags = this.state.selectedTags
+        const id = parseInt(event.target.id)
+        for( var i = 0; i < selectedTags.length; i++){
+            if ( selectedTags[i] === id) {
+                selectedTags.splice(i, 1);
+            }
+        }
+        this.setState({selectedTags})
+    }
+
+    handleChange(event:any) {
+        this.setState({
+            inputField: [event.target.value]
+        })
+    }
+
+    async searchQueryByFilter() {
+        const path = window.location.pathname;
+        const {selectedTags, activeTab} = this.state
+        let queryResult = await getQueryResult(activeTab, selectedTags, path)
+        if (path === '/courses') {
+            this.props.setCourses(queryResult);
+        } else {
+            this.props.setMyCourses(queryResult);
+        }
+        this.props.closeFunction();
+    }
+
+    resetFilter() {
+        window.location.reload()
+        this.props.setFilteringState(false)
     }
 
     render() {
         const close = this.props.closeFunction
-        const {activeTab} = this.state
+        const {activeTab, selectedTags, filtrationTags, inputField} = this.state
+        const filtrationTagsCard = filtrationTags.map((tag: any) => {
+            return (<div onClick={(event) => this.handleAddTag(event)} id={tag[1]}>{tag[0]}</div>)
+        })
+        const selectedTagsCards = filtrationTags.map((tag:any)=> {
+            for (let i = 0; i < selectedTags.length; i++) {
+                if (tag[1] === parseInt(selectedTags[i])) {
+                    return (<div onClick={(event) => this.handleRemoveTag(event)} id={tag[1]}>{tag[0]}</div>)
+                }
+            }
+            return
+        })
+
         const inputDiv = {
             paddingTop: '1rem',
         }
@@ -45,25 +101,25 @@ export class Filter extends React.Component<any, any> {
                 <TrayHeader>Filter:</TrayHeader>
                 <Stack>
                     <Tab
-                        active={activeTab === "Kategori"}
+                        active={activeTab === "category"}
                         onClick={() => this.handleClick('category')}
                     >
                         Kategori
                     </Tab>
                     <Tab
-                        active={activeTab === "Lokasjon"}
+                        active={activeTab === "location"}
                         onClick={() => this.handleClick('location')}
                     >
                         Lokasjon
                     </Tab>
                     <Tab
-                        active={activeTab === "Posisjon"}
+                        active={activeTab === "position"}
                         onClick={() => this.handleClick('position')}
                     >
                         Posisjon
                     </Tab>
                     <Tab
-                        active={activeTab === "Spesialisering"}
+                        active={activeTab === "specialization"}
                         onClick={() => this.handleClick('specialization')}
                     >
                         Spesialisering
@@ -79,10 +135,15 @@ export class Filter extends React.Component<any, any> {
                         fillParent
                         id="search-field"
                         placeholder="Søk her..."
+                        onChange={(event)=>this.handleChange(event)}
+                        value={inputField}
                     />
                 </div>
-                <Text>Tagger:</Text>
+                <Text>Tagger: <div>{selectedTagsCards}</div></Text>
                 <Text>Søkeresultater:</Text>
+                <Text>{filtrationTagsCard}</Text>
+                <Button text="Søk Her!" onClick={()=>this.searchQueryByFilter()}></Button>
+                <Button text="Reset Filter" onClick={()=>this.resetFilter()}></Button>
             </Tray>
         );
     }
