c1200261a12e046e51171a4f5806862a3ed24601
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2021-05-05 12:41:02 +0200

    Added filtration functionality, still otta display stuff in filter tab

--- src/functions/filterFunctions.tsx
+++ src/functions/filterFunctions.tsx
@@ -1,18 +1,21 @@
-export async function getCoursesByFilter(str: string) {
-    let filteredCourses: any;
-    switch(str) {
-        case 'Kategori':
-            filteredCourses = await ['Kategori', 'Kategori'];
-            break;
-        case 'Lokasjon':
-            filteredCourses = await ['Lokasjon', 'Lokasjon'];
-            break;
-        case 'Posisjon':
-            filteredCourses = await ['Posisjon', 'Posisjon'];
-            break;
-        case 'Spesialisering':
-            filteredCourses = await ['Spesialisering', 'Spesialisering'];
-            break;
-    }
-    return filteredCourses;
+const axios = require('axios');
+const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+export async function getFilteredCourses(filter:string, path:string){
+    let url = serverUrl + '/filter'
+    let user_id = localStorage.getItem('session_user_id')
+    const result: any = await axios(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Auth-Token': localStorage.getItem('dossier_session_token')
+        },
+        data: {
+            path: path,
+            filter: filter,
+            user_id: user_id
+
+        }
+    })
+    return result;
 }
